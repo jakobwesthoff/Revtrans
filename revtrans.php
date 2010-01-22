72ba33d17dcd65e4e1dc870af71ad8d4e0cbc201
Jakob Westhoff <jakob@westhoffswelt.de>
2010-01-22 15:00:47 +0100

    Implemented working cli application

--- revtrans.php
+++ revtrans.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Copyright 2010 Jakob Westhoff. All rights reserved.
@@ -34,19 +35,94 @@ include( "classes/password/field.php" );
 include( "classes/password/entry.php" );
 include( "classes/password/entry_list.php" );
 include( "classes/reader/revelation_xml.php" );
+include( "classes/reader/data_provider/revelation_crypto.php" );
+include( "classes/reader/revelation_crypto.php" );
 include( "classes/writer.php" );
 include( "classes/writer/secrets_csv/entry.php" );
 include( "classes/writer/secrets_csv.php" );
 
+echo "RevTrans - Revelation Password File Transformer\n";
+echo "Copyright 2010 Jakob Westhoff\n";
+echo "\n";
+
+
+$options = getopt( 
+    "",
+    array(
+        "input-format:",
+        "password:",
+        "output:",
+    )
+);
+
+if ( $options === false ) 
+{
+    fwrite( STDERR, "Could not parse options correctly.\n" );
+    exit( 2 );
+}
+
+if ( $argc < 2 ) 
+{
+    $basename = basename( $argv[0] );
+    echo <<<USAGE
+Usage: 
+  $basename [OPTIONS] <input file>
+
+
+Options:
+  --input-format=<plain,encrypted>  Input format to read (Default: encrypted)
+  --password=<password>             Password to use for decryption. It is 
+                                    discouraged to supply a password on the 
+                                    commandline. You will be asked for one if
+                                    neccessary.
+  --output=<file>                   Write output to a file instead of stdout.
+
+USAGE;
+
+    exit ( 1 );
+}
+
+// Set default options for the different parameters
+if ( !isset( $options['input-format'] ) )
+{
+    $options['input-format'] = "encrypted";
+}
+if ( !isset( $options['output'] ) )
+{
+    $options['output'] = "php://stdout";
+}
+
+// Maybe a password input is needed
+if ( $options['input-format'] == "encrypted" && ( !isset( $options["password"] ) || $options["password"] === "" ) ) 
+{
+    // Read password without printing it
+    echo "Password: ";
+    system( 'stty -echo' );
+    $options["password"] = trim( fgets( STDIN ) );
+    system( 'stty echo' );
+    // The disabled echo sucked up the newline. Therefore we need to output it 
+    // manually here
+    echo "\n";
+}
+
 try 
 {
-    $reader = new Reader\RevelationXml( $argv[1] );
+    switch( $options["input-format"] ) 
+    {
+        case "plain":
+            $reader = new Reader\RevelationXml( $argv[1] );
+        break;
+        case "encrypted":
+            $reader = new Reader\RevelationCrypto( $argv[1], $options["password"] );
+        break;
+    }
+
     $passwords = $reader->load();
     $writer = new Writer\SecrectsCsv( $passwords );
-    $writer->save( "php://stdout" );
+    $writer->save( $options["output"] );
 }
 catch( \Exception $e ) 
 {
     fwrite( STDERR, $e->getMessage() . "\n" );
-    exit( 1 );
+    exit( 3 );
 }
